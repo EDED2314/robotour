852f93c271329951feccf54a10e1de9777e22e8b
ed.xyz <eddietang2314@gmail.com>
2024-09-23 21:10:42 -0400

    fixed bro

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,17 @@
 #define out_A_IN2 1
 #define out_A_IN1 2
 
-#define out_B_PWM 5
 #define out_B_IN1 6
-#define out_B_IN2 7
+#define out_B_IN2 5
+#define out_B_PWM 7
 
 #define motor_A 0
 #define motor_B 1
 #define motor_AB 2
 
-void motor_speed2(boolean, char);
-void motor_speed(boolean, boolean, byte);
-void motor_brake(boolean);
+void motor_speed2(boolean motor, char speed);
+void motor_speed(boolean motor, boolean direction, byte speed);
+void motor_brake(boolean motor);
 
 void setup()
 {
@@ -26,52 +26,35 @@ void setup()
   pinMode(out_B_PWM, OUTPUT);
   pinMode(out_B_IN1, OUTPUT);
   pinMode(out_B_IN2, OUTPUT);
-
-  // motor_standby(false);
-
-  // motor_speed2(motor_A, -100);
-  // delay(2000);
-  // motor_speed2(motor_A, 0);
-
-  // motor_speed2(motor_A, -100);
-  // motor_speed2(motor_B, -100);
-  // delay(2000);
-  // for (int i = -100; i < 100; i++)
-  // {
-  //     motor_speed2(motor_A, i);
-  //     motor_speed2(motor_B, i);
-  //     delay(50);
-  // }
-  // delay(2000);
-  // motor_standby(true);
 }
 
 void loop()
-
 {
-  motor_speed2(motor_A, -100);
-  motor_speed2(motor_B, -100);
+  motor_speed(motor_A, 0, 100);
+  motor_speed(motor_B, 0, 100);
   delay(2000);
-  motor_speed2(motor_A, 0);
-  motor_speed2(motor_B, 0);
+
+  motor_brake(motor_A);
+  motor_brake(motor_B);
   delay(2000);
 }
 
-void motor_speed2(boolean motor, char speed)
-{ // speed from -100 to 100
-  byte PWMvalue = 0;
-  PWMvalue = map(abs(speed), 0, 100, 50, 255); // anything below 50 is very weak
-  if (speed > 0)
-    motor_speed(motor, 0, PWMvalue);
-  else if (speed < 0)
-    motor_speed(motor, 1, PWMvalue);
-  else
-  {
-    motor_brake(motor);
-  }
-}
+// void motor_speed2(boolean motor, char speed)
+// { // speed from -100 to 100
+//   byte PWMvalue = 0;
+//   PWMvalue = map(abs(speed), 0, 100, 50, 255); // anything below 50 is very weak
+//   if (speed > 0)
+//     motor_speed(motor, 0, PWMvalue);
+//   else if (speed < 0)
+//     motor_speed(motor, 1, PWMvalue);
+//   else
+//   {
+//     motor_brake(motor);
+//   }
+// }
+
 void motor_speed(boolean motor, boolean direction, byte speed)
-{ // speed from 0 to 255
+{ // speed from 0 to 255, direction: 0/false is forward, 1/true
   if (motor == motor_A)
   {
     if (direction == 0)
@@ -101,13 +84,6 @@ void motor_speed(boolean motor, boolean direction, byte speed)
     analogWrite(out_B_PWM, speed);
   }
 }
-// void motor_standby(boolean state)
-// { // low power mode
-//     if (state == true)
-//         digitalWrite(out_STBY, LOW);
-//     else
-//         digitalWrite(out_STBY, HIGH);
-// }
 
 void motor_brake(boolean motor)
 {
@@ -122,18 +98,3 @@ void motor_brake(boolean motor)
     digitalWrite(out_B_IN2, HIGH);
   }
 }
-// void motor_coast(boolean motor)
-// {
-//     if (motor == motor_A)
-//     {
-//         digitalWrite(out_A_IN1, LOW);
-//         digitalWrite(out_A_IN2, LOW);
-//         digitalWrite(out_A_PWM, HIGH);
-//     }
-//     else
-//     {
-//         digitalWrite(out_B_IN1, LOW);
-//         digitalWrite(out_B_IN2, LOW);
-//         digitalWrite(out_B_PWM, HIGH);
-//     }
-// }
